64f94a757a1246e5b839396d919a0bd550c2282e
Neel Smith <dnsmith.neel@gmail.com>
2014-06-06 07:57:18 -0400

    Initial verification of DSE triangular relations.

--- src/main/groovy/org/homermultitext/mom/Verifier.groovy
+++ src/main/groovy/org/homermultitext/mom/Verifier.groovy
@@ -4,6 +4,7 @@ package org.homermultitext.mom
 
 import edu.harvard.chs.cite.CiteUrn
 import edu.harvard.chs.cite.CtsUrn
+import org.homermultitext.citemanager.DseManager
 
 //import groovy.xml.StreamingMarkupBuilder
 
@@ -28,9 +29,49 @@ class Verifier {
 
   public static void main(String[] args) 
   throws Exception {
+    DseManager dsemgr = new DseManager()
     try {
       CiteUrn urn = new CiteUrn (args[0])
       System.out.println "RUNNING VERIFIER on urn " + urn
+
+      String imgFolioStr = args[1].replaceFirst(/^\[/, "")
+      imgFolioStr = imgFolioStr.replaceFirst(/\]$/, "")
+      def imgFolioIndices = imgFolioStr.split(/,/)
+      def imgFolioFiles = []
+      imgFolioIndices.each {  
+	imgFolioFiles.add(new File(it))
+      }
+      dsemgr.tbsImageIndexFiles = imgFolioFiles
+
+
+      String txtImgStr =  args[2].replaceFirst(/^\[/, "")
+      txtImgStr = txtImgStr.replaceFirst(/\]$/, "")
+      def txtImgIndices = txtImgStr.split(/,/)
+      def txtImgFiles = []
+      txtImgIndices.each {  
+	txtImgFiles.add(new File(it))
+      }
+      dsemgr.textImageIndexFiles = txtImgFiles
+
+
+
+      String txtFolioStr =  args[3].replaceFirst(/^\[/, "")
+      txtFolioStr = txtFolioStr.replaceFirst(/\]$/, "")
+      def txtFolioIndices = txtFolioStr.split(/,/)
+      def txtFolioFiles = []
+      txtFolioIndices.each {  
+	txtFolioFiles.add(new File(it))
+      }
+      dsemgr.textTbsIndexFiles = txtFolioFiles
+
+
+      if (dsemgr.verifyTbs(urn)) {
+	System.out.println "All DSE relations are valid."
+      } else {
+	System.out.println "Errors."
+      }
+
+
     } catch (Exception  e) {
       System.err.println "Verifier: bad urn value " + args[0]
       throw e
