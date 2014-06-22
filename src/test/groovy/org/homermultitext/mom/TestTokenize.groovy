b509a906c4a06c530394837e31f53b398ccea49f
Neel Smith <dnsmith.neel@gmail.com>
2014-06-22 12:57:31 -0400

    Add skeleton files for Tokenization.

---
+++ src/test/groovy/org/homermultitext/mom/TestTokenize.groovy
@@ -0,0 +1,13 @@
+package org.homermultitext.mom
+
+
+import static org.junit.Assert.*
+import org.junit.Test
+
+class TestTokenize extends GroovyTestCase {
+
+  void testTokenizing() {
+    assert 1 > 0
+  }
+  
+}
