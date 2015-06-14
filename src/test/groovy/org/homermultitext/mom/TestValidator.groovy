3db0e48fe8bf5e216fa8d266955ce264a3edf302
Neel Smith <dnsmith.neel@gmail.com>
2015-06-14 07:59:10 -0400

    Test HmtValidator's use of LexicalValidation

--- src/test/groovy/org/homermultitext/mom/TestValidator.groovy
+++ src/test/groovy/org/homermultitext/mom/TestValidator.groovy
@@ -10,14 +10,24 @@ class TestValidator extends GroovyTestCase {
 
   File tokens = new File("testdata/tokens/tokens.csv")
   File authSrc = new File("testdata/authlists")
-  
+  File byz = new File("testdata/authlists/orthoequivs.csv")
+  String morphCmd = "../morpheus/bin/morpheus"
+    
   
   
   void testValidator() {
-    HmtValidator v = new HmtValidator(tokens,authSrc)
+    HmtValidator v = new HmtValidator(tokens,authSrc, byz, morphCmd)
     assert v.persv.validates()
     assert v.placev.validates()
     assert v.ethnicv.validates()
+
+    assertFalse v.lexv.validates()
+
+    Integer expectedSuccesses = 390
+    Integer expectedFailures = 111
+    assert v.lexv.successCount() == expectedSuccesses
+    assert v.lexv.failureCount() == expectedFailures
+    
   }
   
 }
