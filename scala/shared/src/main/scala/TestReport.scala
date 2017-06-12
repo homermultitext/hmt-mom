2031722c471d4e415208b03b36aea811a451cc25
Neel Smith <dnsmith.neel@gmail.com>
2017-06-12 05:23:56 -0400

    Placeholder files

---
+++ scala/shared/src/main/scala/TestReport.scala
@@ -0,0 +1,11 @@
+package edu.holycross.shot.hmtmom
+
+import edu.holycross.shot.cite._
+
+
+import scala.scalajs.js
+import js.annotation.JSExport
+
+
+@JSExport case class TestReport (before: String, after: String, success: Boolean) {
+}
---
+++ scala/shared/src/test/scala/TestReportSpec.scala
@@ -0,0 +1,16 @@
+package edu.holycross.shot.hmtmom
+import org.scalatest.FlatSpec
+
+import edu.holycross.shot.cite._
+
+
+/**
+*/
+class TestReportSpec extends FlatSpec {
+
+
+
+  "A TestRerpot" should "do things" in  pending
+
+
+}
