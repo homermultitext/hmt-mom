8ac095659f3a064815d46d14f56f9ce2ca3ad98a
Neel Smith <dnsmith.neel@gmail.com>
2017-06-16 06:21:59 -0400

    Sketch out library building

--- scala/src/main/scala/HmtMom.scala
+++ scala/src/main/scala/HmtMom.scala
@@ -1,11 +1,43 @@
 package edu.holycross.shot.hmtmom
 
 import edu.holycross.shot.cite._
+import edu.holycross.shot.ohco2._
+import edu.holycross.shot.citerelation._
+import edu.holycross.shot.citeobj._
 import edu.holycross.shot.scm._
 
 
 
 object HmtMom {
+  val library = "Editing in progress"
+  val libraryUrn = Cite2Urn("urn:cite2:hmt:mom.2017a:mom")
+  val license = "Creative Commons Attribution Share-Alike"
+  val namespaces = Vector[CiteNamespace]()
+
+
+  /** Build a TextRepository from editorial source.
+  */
+  def texts: Option[TextRepository] = {
+    None
+  }
+
+  /** Build a CiteCollectionRepository from editorial source.
+  */
+  def collections: Option[CiteCollectionRepository] = {
+    None
+  }
+
+  /** Build ImageExtensions from editorial source.
+  */
+  def images: Option[ImageExtensions] = {
+    None
+  }
+
+  /** Build CiteRelationSet from editorial source.
+  */
+  def indexes: Option[CiteRelationSet] = {
+    None
+  }
 
 
   /** Create a CiteLibrary for a directory following
@@ -14,6 +46,14 @@ object HmtMom {
   * @param dir Name of root directory of an editing project.
   */
   def citeLibrary(dir: String): CiteLibrary = {
-    CiteLibrary("","","")
+
+
+
+    CiteLibrary(library,libraryUrn,license,namespaces,
+      texts,collections,images,indexes
+    )
   }
+
+
+
 }
